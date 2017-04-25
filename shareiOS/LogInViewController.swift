535d83570ebc263499a3822863d86fa1f0a78e82
Sunny <sunnylee945@gmail.com>
2017-04-26 01:55:46 +0800

    facebook log in success

--- shareiOS/LogInViewController.swift
+++ shareiOS/LogInViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width-32, height: 50)
+        
+        loginButton.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +28,19 @@ class LogInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
 
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print(error)
+            return
+        }
+        print("Successfully logged in with facebook")
+    }
     /*
     // MARK: - Navigation
 
