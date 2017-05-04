9aab831d5f0fbcd5cd6e767b461023a3c93bd457
Sunny <sunnylee945@gmail.com>
2017-05-05 00:19:34 +0800

    rent vc and car swift file

---
+++ shareiOS/Cars.swift
@@ -0,0 +1,22 @@
+//
+//  Cars.swift
+//  shareiOS
+//
+//  Created by Mac on 2017/5/5.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class Cars: NSObject{
+    
+
+    
+    var carImage: UIImage?
+    var carNumber: String?
+    var oilType: String?
+    var carYear: String?
+    var carType: String?
+
+    
+}
